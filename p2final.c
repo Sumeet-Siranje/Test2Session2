0991dc9427bcb3917a31342dafbb2678a316dc9e
sumeet siranje <94678048+Sumeet-Siranje@users.noreply.github.com>
2022-03-14 05:54:26 +0000

    p2final.c_done

--- p2final.c
+++ p2final.c
@@ -1,1 +1,34 @@
 
+#include<stdio.h>
+#include<math.h>
+void input_line(float *x1,float *y1,float *x2,float *y2,float *x3,float *y3){
+  printf("enter the coordinates\n");
+  scanf("%f%f%f%f%f%f",x1,y1,x2,y2,x3,y3);
+}
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
+{
+  float a,b,c;
+  a=sqrt(pow(x1-x2,2)+pow(y1-y2,2));
+  b=sqrt(pow(x1-x3,2)+pow(y1-y3,2));
+  c=sqrt(pow(x3-x2,2)+pow(y3-y2,2));
+  if(a+b>c && b+c>a && c+a>b)
+    return 1;
+  else
+    return 0;   
+}
+
+void output(float x1, float y1, float x2, float y2,float x3, float y3,int istriangle){
+  if(is_triangle==1){
+    printf("the given side form atriangle\n");
+    }
+  else{
+    printf("the given side dont form a triangle\n");
+    }
+}
+int main(){
+  float x1,y1,x2,y2,x3,y3,triangle;
+  input_line(&x1,&y1,&x2,&y2,&x3,&y3);
+  triangle=is_triangle(x1,y1,x2,y2,x3,y3);
+  output(x1,y1,x2,y2,x3,y3,triangle);
+  return 0;
+}
--- p3final.c
+++ p3final.c
@@ -1,1 +1,31 @@
-
+#include<stdio.h>
+int input_number(){
+  int x;
+  printf("enter the number\n");
+  scanf("%d",&x);
+  return x;
+}
+int is_prime(int n){
+  int value=0;
+  for(int i=1;i<n;i++)
+    {
+      if(n%i==0)
+        value=value+1;
+      else
+        value=value;
+}
+  return value;
+}
+void output(int n,int is_prime){
+  if(is_prime>1)
+    printf("%d is not a prime number\n",n);
+  else
+    printf("%d is a prime number\n",n);
+}
+int main(){
+  int n,prime;
+  n=input_number();
+prime=is_prime(n);
+  output(n,prime);
+  return 0;
+}
